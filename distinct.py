e3ea91cd481e5cd19519e1a06dbed4d3ed9d8ec5
ZacBBedont <zacbbedont@gmail.com>
2025-01-27 14:28:14 -0500

    Add files via upload

---
+++ distinct.py
@@ -0,0 +1,11 @@
+def isDistinct(year: int) -> bool:
+    s = str(year)
+    digits_used = []
+    for char in s:
+        if char in digits_used:
+            return False
+        digits_used.append(char)
+    return True
+year = int(input("Please put in a year")) + 1
+while not isDistinct(year):
+    year += 1
