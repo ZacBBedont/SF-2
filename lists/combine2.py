02e4f35325a02c2f9efd333191fcf36d6e943add
ZacBBedont <zacbbedont@gmail.com>
2025-02-26 15:43:37 -0500

    combine2(homework)

---
+++ lists/combine2.py
@@ -0,0 +1,10 @@
+def combine2(d1,d2):
+    """
+    return dictionary where each key is a key that is a key in the values of d1 and d2
+    the value associated with each key in the new dictionary is the sum of all the integers associated with that key in d1 and d2
+    """
+    
+
+
+d1 = {"a":{3:[2],4:[5,6]},"b":{7:[2,7,9],4:[5,6]}}
+d2 = {"a":{3:[8,12]},"b":{7:[7,33]}}
