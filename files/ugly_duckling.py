64f10cac2f9de5827cbe27c988a239070330c61a
ZacBBedont <zacbbedont@gmail.com>
2025-03-31 15:40:32 -0400

    ugly duckling practice in class

---
+++ files/ugly_duckling.py
@@ -0,0 +1,23 @@
+'''
+print story only to the user
+count the number of words in the story
+'''
+book_txt = open('files/book.txt','r')
+book_lst = book_txt.readlines()
+first = True
+line_len = 0
+for line in range(len(book_lst)):
+    try:
+        book_lst[line+1]
+    except:
+        book_lst = book_lst[:index]
+        break
+    if book_lst[line] == '\n' and first == True:
+        book_lst = book_lst[line:]
+        first = False
+    elif book_lst[line] == '\n':
+        index = line
+for line in book_lst:
+    line_len += len(line.rstrip().split())
+print(line_len)
+    
