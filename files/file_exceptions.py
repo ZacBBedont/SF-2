6340df6f2dc7263d1a8a8ec3059f82fd64b815c8
ZacBBedont <zacbbedont@gmail.com>
2025-03-31 15:40:03 -0400

    file with except example

---
+++ files/file_exceptions.py
@@ -0,0 +1,14 @@
+file_name = 'files/book.txt'
+try:
+    input_file = open(file_name,'r')
+except FileNotFoundError:
+    print('file does not exist')
+    output_file = open(file_name,'w')
+else:
+    for line in input_file:
+        print(line.rstrip())
+input_file = open('files/blah.txt','r')
+blah_lst = input_file.readlines()
+output_file.writelines(blah_lst)
+output_file.close()
+input_file.close()
