16a39453eaf0dc7653e24498f12326fb68010a29
ZacBBedont <zacbbedont@gmail.com>
2025-04-01 12:38:37 -0400

    new ugly duckling + part 3

--- files/ugly_duckling.py
+++ files/ugly_duckling.py
@@ -2,10 +2,12 @@
 print story only to the user
 count the number of words in the story
 '''
-book_txt = open('files/txt filesbook.txt','r')
+book_txt = open('files/txt files/ugly_duckling.txt','r')
 book_lst = book_txt.readlines()
 first = True
-line_len = 0
+words_len = 0
+words_dict = {}
+words_lst = []
 for line in range(len(book_lst)):
     try:
         book_lst[line+1]
@@ -18,6 +20,18 @@ for line in range(len(book_lst)):
     elif book_lst[line] == '\n':
         index = line
 for line in book_lst:
-    line_len += len(line.rstrip().split())
-print(line_len)
-    
+    print(line)
+    words_len += len(line.rstrip().split())
+    words = line.rstrip().split()
+    for word in words:
+        words_dict[word] = words_dict.get(word,0)
+        words_dict[word] += 1
+for word in words_dict:
+    words_lst.append((word,words_dict[word]))
+for pos in range(len(words_lst)):
+    for pos2 in range(len(words_lst)):
+        if words_lst[pos][1] < words_lst[pos2][1]:
+            words_lst[pos],words_lst[pos2] = words_lst[pos2],words_lst[pos]
+
+print(words_len)
+print(words_lst)
