527bce2129205638f51b824a35a6154e8aa4ce1f
zacbbedont <zacbbedont@gmail.com>
2025-04-07 15:29:30 -0400

    assignment 3 in class

---
+++ Assignment3/assignment_3.py
@@ -0,0 +1,48 @@
+"""
+Zaccaria Broomhall Bedont
+Student Id:2438041
+"""
+
+def toCelcius(temp):
+    celcius = round((temp - 32)*(5/9),2)
+    return celcius
+
+def avgTempYear(temp_dict,year):
+    try:
+        values = temp_dict[year]
+        average =  sum(values)/len(values)
+    except KeyError:
+        print("sorry! That is not a year in our data.")
+    else:
+        return round(average,2)
+
+def topThreeYears(temp_dict):
+    avg_lst = []
+    for year in temp_dict:
+        average = avgTempYear(temp_dict,year)
+        avg_lst.append(average)
+    for temp_pos in range(len(avg_lst)):
+        for other_temp in range(len(avg_lst)):
+                if avg_lst[other_temp] <= avg_lst[temp_pos]:
+                    avg_lst[other_temp],avg_lst[temp_pos] = avg_lst[temp_pos],avg_lst[other_temp]
+    return avg_lst[:3]
+
+def avgTempMonth(temp_dict,month):
+    month_pos = {'JAN':1,'FEB':2,'MAR':3,'APR':4,'MAY':5,'JUN':6,'JUL':7,'AUG':8,'SEP':9,'OCT':10,'NOV':11,'DEC':12}
+    month_lst = []
+    for value in temp_dict.values():
+        month_lst.append(value[month_pos[month]-1])
+    month_avg = sum(month_lst)/len(month_lst)
+    return round(month_avg,2)
+
+input_file = open('Assignment3/data.txt','r')
+temp_dict = {}
+
+for line in input_file:
+    line = line.split()
+    if len(line) > 0 and line[0].isdigit():
+        temp_floats = list(map(float,line[1:]))
+        temp_dict[int(line[0])] = list(map(toCelcius,temp_floats))
+
+
+
