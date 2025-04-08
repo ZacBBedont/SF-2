c51b2b823b8bbb2cfdd9ec897cd5781ab4567129
ZacBBedont <zacbbedont@gmail.com>
2025-04-08 13:03:00 -0400

    point part 1

---
+++ Objects/point1.py
@@ -0,0 +1,11 @@
+class Point:
+    def __init__(self):
+        '''
+        creating a 2-d point at (0,0)
+        '''
+        self.x = 0
+        self.y = 0
+
+###############################
+p1 = Point()
+print(p1.x,p1.y)
